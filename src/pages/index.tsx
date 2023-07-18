85295dafe09c5654dec9e78d41cc805fb931ed5e
muzamilkhan <k.muzamil1001@gmail.com>
2023-07-18 17:09:15 -0400

    Error handling

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,14 +1,22 @@
-import { signIn, signOut, useSession } from "next-auth/react";
-import Head from "next/head";
-import Link from "next/link";
-import { api } from "~/utils/api";
 import React, { useState, FormEvent } from "react";
 import Filter from "~/components/Filter";
 
+interface Business {
+  image_url: string;
+  name: string;
+  location: {
+    address1: string;
+  };
+  price: string;
+  rating: number;
+  phone: string;
+}
+
 export default function Home() {
   const [zipCode, setZipCode] = useState("");
-  const [halalBusinesses, setHalalBusinesses] = useState([]);
+  const [halalBusinesses, setHalalBusinesses] = useState<Business[]>([]);
   const [loadingBusinesses, setLoadingBusinesses] = useState(false);
+  const [noBusinesses, setNoBusinesses] = useState(false);
 
   const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
     e.preventDefault();
@@ -17,13 +25,20 @@ export default function Home() {
       setLoadingBusinesses(true);
 
       const response = await fetch(`/api/halalfood?zipcode=${zipCode}`);
-      const data = await response.json();
-      await new Promise((resolve) => setTimeout(resolve, 1000));
+      if (response.status === 500) {
+        setNoBusinesses(true);
+      } else {
+        const data = await response.json();
+        await new Promise((resolve) => setTimeout(resolve, 1000));
 
-      setHalalBusinesses(data);
-      console.log(data);
+        setHalalBusinesses(data);
+        console.log(data);
+
+        setNoBusinesses(data.length === 0);
+      }
     } catch (error) {
       console.error("Error retrieving halal food businesses:", error);
+      setNoBusinesses(false);
     } finally {
       setLoadingBusinesses(false);
     }
@@ -81,7 +96,7 @@ export default function Home() {
               <>
                 <svg
                   aria-hidden="true"
-                  className="mr-2 h-8 w-8 animate-spin fill-blue-600 text-gray-200 dark:text-gray-600"
+                  className="mr-2 h-10 w-10 animate-spin fill-white text-blue-500 "
                   viewBox="0 0 100 101"
                   fill="none"
                   xmlns="http://www.w3.org/2000/svg"
@@ -100,31 +115,57 @@ export default function Home() {
             )}
           </div>
           <div className="flex flex-wrap justify-center">
-            {halalBusinesses.map((business: any) => (
-              <div className="mx-2 mb-4 flex w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow">
-                <div className=" flex flex-col  p-5">
-                  <img className="h-20 w-20" src={business.image_url} alt="" />
-                  <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
-                    {business.name}
-                  </h5>
-                  <p className="mb-3 font-normal text-gray-700">
-                    {business.location.address1}
-                  </p>
-                  <p className="mb-3 font-normal text-gray-700">
-                    {business.price}
-                  </p>
-                  <p className="mb-3 font-normal text-gray-700">
-                    ⭐ {business.rating}
-                  </p>
-                  <a
-                    href="#"
-                    className="inline-flex w-fit items-center rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300"
+            {halalBusinesses.length > 0
+              ? halalBusinesses.map((business: Business) => (
+                  <div
+                    className="mx-2 mb-4 flex w-full max-w-sm rounded-lg border border-gray-200 bg-white shadow"
+                    key={business.name}
                   >
-                    {business.phone}{" "}
-                  </a>
-                </div>
-              </div>
-            ))}
+                    <div className="flex flex-col p-5">
+                      <img
+                        className="h-20 w-20"
+                        src={business.image_url}
+                        alt=""
+                      />
+                      <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900">
+                        {business.name}
+                      </h5>
+                      <p className="mb-3 font-normal text-gray-700">
+                        {business.location.address1}
+                      </p>
+                      <p className="mb-3 font-normal text-gray-700">
+                        {business.price}
+                      </p>
+                      <p className="mb-3 font-normal text-gray-700">
+                        ⭐ {business.rating}
+                      </p>
+                      <button className="inline-flex w-fit items-center gap-3 rounded-lg bg-blue-700 px-3 py-2 text-center text-sm font-medium text-white hover:bg-blue-800 focus:outline-none focus:ring-4 focus:ring-blue-300">
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
+                          fill="currentColor"
+                          className="h-6 w-6"
+                        >
+                          <path
+                            fill-rule="evenodd"
+                            d="M15 3.75a.75.75 0 01.75-.75h4.5a.75.75 0 01.75.75v4.5a.75.75 0 01-1.5 0V5.56l-4.72 4.72a.75.75 0 11-1.06-1.06l4.72-4.72h-2.69a.75.75 0 01-.75-.75z"
+                            clip-rule="evenodd"
+                          />
+                          <path
+                            fill-rule="evenodd"
+                            d="M1.5 4.5a3 3 0 013-3h1.372c.86 0 1.61.586 1.819 1.42l1.105 4.423a1.875 1.875 0 01-.694 1.955l-1.293.97c-.135.101-.164.249-.126.352a11.285 11.285 0 006.697 6.697c.103.038.25.009.352-.126l.97-1.293a1.875 1.875 0 011.955-.694l4.423 1.105c.834.209 1.42.959 1.42 1.82V19.5a3 3 0 01-3 3h-2.25C8.552 22.5 1.5 15.448 1.5 6.75V4.5z"
+                            clip-rule="evenodd"
+                          />
+                        </svg>
+
+                        {business.phone}
+                      </button>
+                    </div>
+                  </div>
+                ))
+              : noBusinesses && (
+                  <p className="text-black">No halal businesses found.</p>
+                )}
           </div>
         </section>
       </main>
